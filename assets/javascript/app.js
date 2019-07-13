2a405c3ca4b185cea4426e3a5ebb6252d827fe5f
Colin Elving <viewtifuljho@hotmail.com>
2019-07-13 00:51:58 -0500

    giphy api functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,30 @@
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hntKDD7xWMfeGx2a1zprRXvz8whJ8mSA&limit=10"
+var topics = ["cats"];
 
+var searchTerm = topics[0];
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hntKDD7xWMfeGx2a1zprRXvz8whJ8mSA&q=" + searchTerm + "&limit=10";
+
+$(document).ready(function() {
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        // Saving the original_still property
+        for (var i = 0; i <10; i ++){
+        var imageUrl = response.data[i].images.fixed_height_still.url;
+
+        // Creating and storing an image tag
+        var image = $("<img>");
+
+        // Setting the image src attribute to imageUrl
+        image.attr("src", imageUrl);
+
+        // Prepending the image to the images div
+        $("#image-area").prepend(image);
+    }
+
+    });
+
+});
